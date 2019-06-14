cf64c59a8f4a9ef950ebd4ff9144053d200bc586
liutaochange <liutaochange@163.com>
2019-06-14 19:13:58 +0800

    feat: dart string类型学习

---
+++ dart/chapter2/string.dart
@@ -0,0 +1,14 @@
+void main () {
+  print("i am string");
+  String d = "arm";
+  print(d);
+  // \n 实现换行
+  String s = "money is much \n add more";
+  print(s);
+  // r 不转义
+  String s2 = r"money is much \n add more";
+  print(s2);
+  // * 实现重复 
+  String s4 = "this is test";
+  print(s4 * 5);
+}
