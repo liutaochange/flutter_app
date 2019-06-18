98b0d5263847ed5deb32a5587b9b07a7695a2796
liutaochange <liutaochange@163.com>
2019-06-18 21:05:41 +0800

    feat: 关系运算符

---
+++ dart/chapter3/relation.dart
@@ -0,0 +1,14 @@
+void main () {
+  print("relation");
+  // 关系运算符 
+  // == != > < <= >=
+  int a = 100;
+  int b = 200;
+  print(a == b);
+  print(a != b);
+  print(a >= b);
+
+  var stra = "stringa";
+  var strb = "stringb";
+  print(stra == strb);
+}
