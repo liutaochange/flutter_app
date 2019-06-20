9afbc041b57312194379f3e132b68e914bebc97f
liutaochange <liutaochange@163.com>
2019-06-20 17:55:03 +0800

    feat: 控制语句

---
+++ dart/chapter4/for.dart
@@ -0,0 +1,7 @@
+void main() {
+  print("for");
+  List a = [1,2,3,4,5,6];
+  for(var i = 0; i < a.length; i++){
+    print(a[i]);
+  }
+}
---
+++ dart/chapter4/if.dart
@@ -0,0 +1,13 @@
+void main () {
+  print("if");
+  var isTrue = true;
+  if (isTrue) {
+    print(true);
+  }
+  int count = 100;
+  if (count >= 60) {
+    print("及格");
+  } else if (count < 60) {
+    print("不及格");
+  }
+}
