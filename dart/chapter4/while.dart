01f08761b9854c5974644de2357f68f33868f827
liutaochange <liutaochange@163.com>
2019-06-22 20:53:10 +0800

    feat: 循环语句

--- dart/chapter4/while.dart
+++ dart/chapter4/while.dart
@@ -1,3 +1,14 @@
 void main() {
   print("while");
+  int count = 0;
+  while(count < 5) {
+    count++;
+    print(count);
+  }
+  print("------");
+
+  do{
+    count++;
+    print(count);
+  }while(count < 10)
 }
