17a6bbdb89c011008e8d607af7ebfc9a60051dcb
liutaochange <liutaochange@163.com>
2019-06-23 21:42:24 +0800

    feat: function类型

--- dart/chapter5/function.dart
+++ dart/chapter5/function.dart
@@ -1,8 +1,11 @@
 void main() {
   print("function");
   print(getPerson("张三", 18));
+  print(sum(10, 35));
 }
 
 String getPerson(name, age) {
   return "name=$name, age=$age";
 }
+
+Number sum = (Number a, Number b) => a + b;
