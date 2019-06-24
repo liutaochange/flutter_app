e8b0cb794a84ac49e178b27f6343cce1ab9662aa
liutaochange <liutaochange@163.com>
2019-06-24 21:23:49 +0800

    feat: default arguments

---
+++ dart/chapter5/default.dart
@@ -0,0 +1,16 @@
+// 默认参数
+// 使用=在可选参数指定默认值
+// 默认值只能是编译时常量
+void main() {
+  print("default");
+
+  output(String name,{int age = 18,String gender = "女"}){
+    print("name=$name,age=$age,gender=$gender");
+  }
+  output("dart");
+  console(int a, [String b = "web", bool c = true]) {
+    print("a=$a,b=$b,c=$c");
+  }
+  console(100);
+  console(100, "java", false);
+}
