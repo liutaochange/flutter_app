1d84c5eadcde265838aa9dae27471712f37a4c10
liutaochange <liutaochange@163.com>
2019-06-28 00:00:22 +0800

    feat: closure

--- dart/chapter5/closure.dart
+++ dart/chapter5/closure.dart
@@ -1,3 +1,13 @@
+// 闭包是为了 在某个方法外部获取该方法内的局部变量的值 比如这里面的 count，且 多次调用该闭包 闭包是会对count的值进行保存的 所以打印的数值是递增的。
+
+// 闭包函数的返回值一定是一个函数
 void main() {
   print("closure")
+  //定义方法
+  a(){
+    int count=0;
+    printCount(){
+      print(count++);
+    }
+  }
 }
