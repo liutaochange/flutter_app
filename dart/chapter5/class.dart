5681bfabfcd571587bdced95ce00a72544657a33
liutaochange <liutaochange@163.com>
2019-06-29 23:29:07 +0800

    feat: 新增注释

--- dart/chapter5/class.dart
+++ dart/chapter5/class.dart
@@ -1,3 +1,9 @@
+// 一，类与对象
+// 使用关键字class声明一个类
+// 使用关键字new创建一个对象，new 可省略（与java不同）
+// 所有对象都继承于Object类
+// 二，属性与方法
+// 属性默认会生成getter和setter方法
 void main() {
   print("class");
   var people = new Person();
