08985ff19ddfd8c67b55a633f2a552f0bb567e14
liutaochange <liutaochange@163.com>
2019-07-01 18:29:24 +0800

    feat: color插件的应用

---
+++ lib/less_status.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+// StatelessWidget 基础组件
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
