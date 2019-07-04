9f9e08684b417268efba42b37aa0ca20ab1f8c53
liutaochange <liutaochange@163.com>
2019-07-04 19:18:08 +0800

    feat: 自定义组件

--- lib/list_horizontal.dart
+++ lib/list_horizontal.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ListApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Horizontal Listview Page',
@@ -13,26 +14,33 @@ class ListApp extends StatelessWidget {
           child: Container(
             height: 200,
             width: 300,
-            child: new ListView(
-              scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                new Image.network(
-                  'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=360700842,971150135&fm=15&gp=0.jpg',
-                  width: 300,
-                ),
-                new Image.network(
-                  'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=882455271,3138119339&fm=15&gp=0.jpg',
-                  width: 300,
-                ),
-                new Image.network(
-                  'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=343829608,968476013&fm=15&gp=0.jpg',
-                  width: 300,
-                )
-              ],
-            ),
+            child: MyList,
           ),
         ),
       ),
+    );
+  }
+}
+
+class MyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        new Image.network(
+          'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=360700842,971150135&fm=15&gp=0.jpg',
+          width: 300,
+        ),
+        new Image.network(
+          'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=882455271,3138119339&fm=15&gp=0.jpg',
+          width: 300,
+        ),
+        new Image.network(
+          'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=343829608,968476013&fm=15&gp=0.jpg',
+          width: 300,
+        )
+      ],
     );
   }
 }
