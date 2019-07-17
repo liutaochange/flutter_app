c5fbb2377d1df45c9c8dc1558b5b183364883d02
liutaochange <liutaochange@163.com>
2019-07-17 22:14:32 +0800

    feat: dart知识回顾

---
+++ dart/chapter7/play.dart
@@ -0,0 +1,18 @@
+void main() {
+  print('dart');
+
+  final date = new DateTime.now();
+  print(date);
+
+  String str = 'i am string';
+  print(str);
+
+  String str1 = '''
+  i am str1
+  i am str2
+  i am str3
+  ''';
+  print(str1);
+
+  // 字符串通过$str $str1 拼接
+}
